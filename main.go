26be5734689c86401af67f8e18c1179215daf64e
Andrew Snodgrass <pointlander@gmail.com>
2023-11-30 19:38:43 -0600

    Added Net

--- main.go
+++ main.go
@@ -8,8 +8,137 @@ import (
 	"compress/bzip2"
 	"fmt"
 	"io/ioutil"
+	"math"
+	"math/rand"
 	"os"
+	"sort"
+	"sync/atomic"
 )
+
+// Random is a random variable
+type Random struct {
+	Mean   float32
+	StdDev float32
+}
+
+// Net is a net
+type Net struct {
+	window       int64
+	Inputs       int
+	Outputs      int
+	Rng          *rand.Rand
+	Distribution [][]Random
+}
+
+// NewNet makes a new network
+func NewNet(seed int64, window int64, inputs, outputs int) Net {
+	rng := rand.New(rand.NewSource(seed))
+	distribution := make([][]Random, outputs)
+	for i := range distribution {
+		for j := 0; j < inputs; j++ {
+			distribution[i] = append(distribution[i], Random{
+				Mean:   0,
+				StdDev: 1,
+			})
+		}
+	}
+	return Net{
+		window:       window,
+		Inputs:       inputs,
+		Outputs:      outputs,
+		Rng:          rng,
+		Distribution: distribution,
+	}
+}
+
+// Set window sets the window
+func (n *Net) SetWindow(window int64) {
+	atomic.StoreInt64(&n.window, window)
+}
+
+// Sample is a sample of a random neural network
+type Sample struct {
+	Entropy float32
+	Neurons []Matrix
+	Outputs Matrix
+}
+
+// Fire runs the network
+func (n *Net) Fire(input Matrix) Matrix {
+	rng, distribution, window := n.Rng, n.Distribution, atomic.LoadInt64(&n.window)
+	output := NewMatrix(0, n.Outputs, Samples)
+
+	systems := make([]Sample, 0, 8)
+	for i := 0; i < Samples; i++ {
+		neurons := make([]Matrix, n.Outputs)
+		for j := range neurons {
+			neurons[j] = NewMatrix(0, n.Inputs, 1)
+			for k := 0; k < n.Inputs; k++ {
+				v := float32(rng.NormFloat64())*distribution[j][k].StdDev + distribution[j][k].Mean
+				if v > 0 {
+					v = 1
+				} else {
+					v = -1
+				}
+				neurons[j].Data = append(neurons[j].Data, v)
+			}
+		}
+		outputs := NewMatrix(0, n.Outputs, 1)
+		for j := range neurons {
+			out := MulT(neurons[j], input)
+			output.Data = append(output.Data, out.Data[0])
+			outputs.Data = append(outputs.Data, out.Data[0])
+		}
+		systems = append(systems, Sample{
+			Neurons: neurons,
+			Outputs: outputs,
+		})
+	}
+	entropies := SelfEntropy(output, output, output)
+	for i, entropy := range entropies {
+		systems[i].Entropy = entropy
+	}
+	sort.Slice(entropies, func(i, j int) bool {
+		return systems[i].Entropy < systems[j].Entropy
+	})
+	next := make([][]Random, n.Outputs)
+	for i := range next {
+		for j := 0; j < n.Inputs; j++ {
+			next[i] = append(next[i], Random{
+				Mean:   0,
+				StdDev: 0,
+			})
+		}
+	}
+	for i := range systems[:window] {
+		for j := range systems[i].Neurons {
+			for k, value := range systems[i].Neurons[j].Data {
+				next[j][k].Mean += value
+			}
+		}
+	}
+	for i := range next {
+		for j := range next[i] {
+			next[i][j].Mean /= float32(window)
+		}
+	}
+	for i := range systems[:window] {
+		for j := range systems[i].Neurons {
+			for k, value := range systems[i].Neurons[j].Data {
+				diff := next[j][k].Mean - value
+				next[j][k].StdDev += diff * diff
+			}
+		}
+	}
+	for i := range next {
+		for j := range next[i] {
+			next[i][j].StdDev /= float32(window)
+			next[i][j].StdDev = float32(math.Sqrt(float64(next[i][j].StdDev)))
+		}
+	}
+	n.Distribution = next
+	return systems[0].Outputs
+}
 
 func main() {
 	input, err := os.Open("10.txt.utf-8.bz2")
